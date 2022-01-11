18e5d61b785ce61761fdd91426b679a0807363d3
Zia Gondal <89640018+ziabinumer@users.noreply.github.com>
2022-01-11 17:00:47 +0500

    Create bubble_sort.c

---
+++ C/bubble_sort.c
@@ -0,0 +1,36 @@
+// CTRL + ALT + R
+#include <stdio.h>
+
+int main(void)
+{
+   int n;
+   printf("Size of Array: ");
+   scanf("%d", &n);
+   int array[n];
+   printf("Array: ");
+   for (int i = 0; i < n; i++)
+   {
+       
+       scanf("%d", &array[i]);
+   }
+
+   int _1, _2;
+   for (int l = 0; l < n - 1; l++)
+   {
+       for (int i = 0; i < n - 1; i++)
+       {
+           if (array[i] > array[i + 1])
+           {
+               _1 = array[i + 1];
+               _2 = array[i];
+               array[i] = _1;
+               array[i + 1] = _2;
+           }
+        }
+   }
+    // ("Biggest Number: %d\n", array[n - 1]);
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d   ", array[i]);
+    printf("\n");
+    return 0;
+}
